e4a3d5b536726eea5d46e573b4945e458ab14fd7
Bizmarky <memccallum98@gmail.com>
2017-04-01 03:00:25 -0400

    Finished Task One

---
+++ Test One/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	//Speed variable for cube movement
+	private float speed = 20f;
+
+	//Color variable and renderer for the color of the cube
+	public Color color;
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+		//Assign the color variable and the renderer
+		color = gameObject.GetComponent<Renderer> ().material.color;
+		rend = GetComponent<Renderer> ();
+		rend.material.color = color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Gets input from the arrow keys and moves the cube
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Vector3 move = new Vector3 (horizontal, 0, vertical);
+		rb.AddForce (move * speed);
+
+
+
+		//The following are buttons 1, 2, 3 corresponding to yellow, blue, green
+		if (Input.GetKeyDown ("1")) 
+		{
+			color = Color.yellow;
+		}		
+		if (Input.GetKeyDown ("2")) 
+		{
+			color = Color.blue;
+		}
+		if (Input.GetKeyDown ("3")) 
+		{
+			color = Color.green;
+		}
+		rend.material.color = color;
+	}
+}
---
+++ Test One/Assets/Scripts/Spawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	GameObject alive;
+
+	// Use this for initialization
+	void Start () {
+		alive = GameObject.Find ("Player");
+		alive.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) 
+		{
+			alive.SetActive (true);
+		}
+		if (Input.GetKeyDown ("backspace")) 
+		{
+			alive.SetActive (false);
+		}
+	}
+}
